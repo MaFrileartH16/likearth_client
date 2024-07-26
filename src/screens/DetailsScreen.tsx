e4154f1e0fbb5cbab2cfbbadbd1f55ca55503035
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2024-07-26 17:37:55 +0700

    add navigation

---
+++ src/screens/DetailsScreen.tsx
@@ -0,0 +1,23 @@
+// screens/DetailsScreen.js
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const DetailsScreen = ({ navigation } : any) => (
+	<View style={styles.container}>
+		<Text>Details Screen</Text>
+		<Button
+			title="Go back"
+			onPress={() => navigation.goBack()}
+		/>
+	</View>
+);
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
+
+export default DetailsScreen;
